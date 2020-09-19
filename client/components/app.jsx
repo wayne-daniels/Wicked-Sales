cd01079a07177e9e5997052e9df5b0d3b95059df
wayne-daniels <wa.daniels@hotmail.com>
2020-09-19 18:38:16 +0000

    Commit Feature 7 work due to restart of desktop

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
 import ProductDetails from './product-details';
+import CartSummary from './cart-summary';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -80,6 +81,13 @@ export default class App extends React.Component {
           <ProductDetails product={this.props.product}
             setView={this.setView} viewParams={this.state.view.params}
             addToCart={this.addToCart} />
+        </div>
+      );
+    } else if (viewName === 'cart') {
+      return (
+        <div className="bg-light">
+          <Header setView={this.setView} cartItemCount={this.state.cart.length} />
+          <CartSummary cart={this.state.cart} setView={this.setView} />
         </div>
       );
     }
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,6 +1,48 @@
-// import React from 'react';
-// import CartSummaryItem from './cart-summary-item';
+import React from 'react';
+import CartSummaryItem from './cart-summary-item';
 
-// export default function CartSummary(props) {
+export default function CartSummary(props) {
+  if (props.cart.length === 0) {
+    return (
+      <div className="bg-warning">
+        <div
+          className="text-primary mb-4 pt-2 px-0 btn d-flex justify-content-center"
+          style={{ cursor: 'pointer' }}
+          onClick={() => props.setView('catalog', {})}>
+          &lt; Back to Catalog
+        </div>
+        <h3 className="text-center text-danger pb-5"></h3>
+      </div>
+    );
+  }
 
-// }
+  const cartItems = props.cart.map(item =>
+    <CartSummaryItem item={item} key={item.productId} />
+  );
+  const totalPrice = props.cart.reduce((acc, curr) => acc + curr.price, 0);
+
+  return (
+    <div className="row">
+      <div className="d-flex flex-column col-7 mx-auto">
+        <div
+          className="text-muted mb-4 pt-2 px-0 btn d-flex justify-content-start"
+          style={{ cursor: 'pointer' }}
+          onClick={() => props.setView('catalog', {})}>
+          &lt; Back to Catalog
+        </div>
+        <h2 className="mb-4">My Cart</h2>
+        {cartItems}
+        <div className="mt-3 mb-5 d-flex justify-content-between">
+          <h5 className="text-muted d-flex align-items-center">
+            Total Price: <span className="text-success">${((totalPrice) * 0.01).toFixed(2)}</span>
+          </h5>
+          <div>
+            <button type="button" className="btn btn-success btn-lg" onClick={() => props.setView('checkout', {})}>
+              Checkout
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
